42decec6774c9997e77f55262eae67d1da298e27
rchudasa <ruchi.chudasama@cern.ch>
2022-11-23 13:18:03 +0100

    Gjet sim script

--- produceMC/crabConfig_GJet_SIM.py
+++ produceMC/crabConfig_GJet_SIM.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 #config.section_('General')
-config.General.requestName = 'GJet_Pt-20to40_AODSIM'
+config.General.requestName = 'GJet_Pt-20to40_SIM'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -12,16 +12,16 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'sim_GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8.py'
 #config.Data.outputPrimaryDataset = 'GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8'
 
-config.Data.inputDBS = 'global'
+config.Data.inputDBS = 'phys03'
 config.JobType.allowUndistributedCMSSW = True
 #config.JobType.numCores = 8
-config.Data.inputDataset ='/GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8/RunIISummer19UL18GEN-106X_upgrade2018_realistic_v4-v1/GEN'
+config.Data.inputDataset ='/GJet_Pt-20to40_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8/phys_heavyions-crab_GJet_Pt-20to40_GENv3-99d9aa36932aa71f9045d950d2e50dc5/USER'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1 
 
-config.Data.outLFNDirBase = '/store/group/phys_heavyions/rchudasa/e2e'
+config.Data.outLFNDirBase = '/store/group/lpcml/rchudasa/'
 #config.Data.outLFNDirBase = '/store/user/rchudasa/'
 config.Data.publication = True 
-config.Site.storageSite = 'T2_CH_CERN'
-#config.Site.storageSite = 'T3_US_FNALLPC'
+#config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T3_US_FNALLPC'
 config.Data.outputDatasetTag = config.General.requestName
