70c99ccc4e3d5b1f8fd7c13d41408e0da304c493
rchudasa <ruchi.chudasama@cern.ch>
2023-09-05 11:22:25 +0200

    scp cross-checks

---
+++ top_gun_opendata.py
@@ -0,0 +1,143 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.381.2.28 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/Guns/pigun_cfi.py --step GEN,SIM,DIGI,L1,DIGI2RAW,HLT:7E33v2,RAW2DIGI,L1Reco,RECO --beamspot Realistic8TeVCollision --conditions START53_V27::All --pileup NoPileUp --datamix NODATAMIXER --eventcontent AODSIM --datatier AODSIM -n 5 --no_exec --fileout file:step_AODSIM_noPU.root --python_filename DoubleGunPt15To100_noPU_LHE2AOD_cfg.py
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('HLTrigger.Configuration.HLT_7E33v2_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(200)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.381.2.28 $'),
+    annotation = cms.untracked.string('Configuration/GenProduction/python/Guns/pigun_cfi.py nevts:5'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    outputCommands = process.AODSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('file:step_AODSIM_noPU.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('AODSIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V27::All', '')
+
+process.generator = cms.EDFilter("Pythia8PtGunV3",
+    PGunParameters = cms.PSet(
+
+        #top quark & antitop
+        ParticleID = cms.vint32(6),
+        AddAntiParticle = cms.bool(True),
+        
+        #phi
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        
+        #eta
+        MinEta = cms.double(-1.4),
+        MaxEta = cms.double(1.4),
+        
+        #mass range
+        MinMass = cms.double(85.0),
+        MaxMass = cms.double(500.0),
+
+        #pt range
+        MinPt = cms.double(400.0),
+        MaxPt = cms.double(1000.0),
+
+    ),
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('processParameters'),
+        processParameters = cms.vstring(
+            '6:onMode = off', #turn off t decays
+            '6:onIfMatch = 5 24', #turn on t decays to b W
+            '24:onMode = off', #turn off W decays
+            '24:onIfAny = 1 2 3 4 5 6') #turn on W decays to quarks
+    ),
+    Verbosity = cms.untracked.int32(0),
+    firstRun = cms.untracked.uint32(1),
+    maxEventsToPrint = cms.untracked.int32(1),
+    psethack = cms.string('double pi0 pt'),
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.AODSIMoutput_step])
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
+
+#call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforMC(process)
+
+# End of customisation functions
